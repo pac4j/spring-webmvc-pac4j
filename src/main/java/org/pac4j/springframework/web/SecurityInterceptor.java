99f8f7621d9edebab89562f2e2f1b790b1329827
Jérôme LELEU <leleuj@gmail.com>
2019-03-18 08:18:18 +0100

    Easy config for authorizers and matchers

--- src/main/java/org/pac4j/springframework/web/SecurityInterceptor.java
+++ src/main/java/org/pac4j/springframework/web/SecurityInterceptor.java
@@ -1,13 +1,17 @@
 package org.pac4j.springframework.web;
 
+import org.pac4j.core.authorization.authorizer.Authorizer;
 import org.pac4j.core.config.Config;
 import org.pac4j.core.context.J2EContext;
 import org.pac4j.core.engine.DefaultSecurityLogic;
 import org.pac4j.core.engine.SecurityLogic;
+import org.pac4j.core.matching.Matcher;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.pac4j.core.util.CommonHelper.assertNotNull;
 
@@ -22,6 +26,8 @@ import static org.pac4j.core.util.CommonHelper.assertNotNull;
  * @since 1.0.0
  */
 public class SecurityInterceptor extends HandlerInterceptorAdapter {
+
+    private static final AtomicInteger internalNumber = new AtomicInteger(1);
 
     private SecurityLogic<Boolean, J2EContext> securityLogic = new DefaultSecurityLogic<>();
 
@@ -49,9 +55,49 @@ public class SecurityInterceptor extends HandlerInterceptorAdapter {
         this.authorizers = authorizers;
     }
 
+    public SecurityInterceptor(final Config config, final String clients, final Authorizer[] authorizers) {
+        this(config, clients);
+        this.authorizers = addAuthorizers(config, authorizers);
+    }
+
     public SecurityInterceptor(final Config config, final String clients, final String authorizers, final String matchers) {
         this(config, clients, authorizers);
         this.matchers = matchers;
+    }
+
+    public SecurityInterceptor(final Config config, final String clients, final Authorizer[] authorizers, final Matcher[] matchers) {
+        this(config, clients, addAuthorizers(config, authorizers));
+        this.matchers = addMatchers(config, matchers);
+    }
+
+    private static String addAuthorizers(final Config config, final Authorizer[] authorizers) {
+        final int n = internalNumber.getAndAdd(1);
+        final int nbAuthorizers = authorizers.length;
+        final StringBuilder names = new StringBuilder("");
+        for (int i = 0; i < nbAuthorizers; i++) {
+            final String name = "$int_authorizer" + n + "." + i;
+            config.addAuthorizer(name, authorizers[i]);
+            if (i > 0) {
+                names.append(",");
+            }
+            names.append(name);
+        }
+        return names.toString();
+    }
+
+    private static String addMatchers(final Config config, final Matcher[] matchers) {
+        final int n = internalNumber.getAndAdd(1);
+        final int nbMatchers = matchers.length;
+        final StringBuilder names = new StringBuilder("");
+        for (int i = 0; i < nbMatchers; i++) {
+            final String name = "$int_matcher" + n + "." + i;
+            config.addMatcher(name, matchers[i]);
+            if (i > 0) {
+                names.append(",");
+            }
+            names.append(name);
+        }
+        return names.toString();
     }
 
     @Override
